7be4e83e7fdd92802880618cbfae166518480975
Lucas Guedes <lucasguedescorreia01@gmail.com>
2023-12-10 17:52:34 -0300

    update

--- DivisaoSegundaForma.asm
+++ DivisaoSegundaForma.asm
@@ -1,4 +1,20 @@
 .text
+.eqv printStringService 4
+.eqv printIntService 1
+
+.macro imprimeString(%endString)
+            la      $a0, %endString
+            li      $v0, printStringService
+            syscall
+.end_macro
+
+.macro imprimeInt(%endInt)
+            la      $a0, %endInt
+            lw	    $a0, 0($a0)
+            li      $v0, printIntService
+            syscall
+.end_macro
+
 # Programa principal
     main:
         # Carregar os valores de x e y nos registradores
@@ -7,75 +23,91 @@
 
         # Chamar o procedimento de divisão
         jal div32
-
-        # Exibir o resultado
-        move $a0, $v0
-        li $v0, 1
-        syscall
+        move $t0, $v0
+        move $t1, $v1
+        sw $t0, quociente
+        sw $t1, resto
+	imprimeString(quocienteString)
+	imprimeInt(quociente)
+	imprimeString(restoString)
+	imprimeInt(resto)	
         
         #encerrando
         li $v0, 10       # Código do sistema para encerrar o programa
         syscall
 # Procedimento de divisão
 div32:
-	addiu $sp, $sp, -16
+	addiu $sp, $sp, -20
 	sw $s0, 4($sp)
 	sw $s1, 8($sp)
 	sw $s3, 12($sp)
+	sw $s4, 16($sp)
 	sw $ra, 0($sp)
 	
-	move $s0, $a0 #resto = x
+	move $s0, $a0 #resto_low = x
+	li $s4, 0 #resto_high
 	move $s1, $a1 #divisor = y
 	
-	sll $s0, $s0, 1 #shift para esquerda 1 vez
-	sll $s1, $s1, 4 #shift para esquerda 4 vezes
+	srl $t0, $s0, 31	# pega MSB do resto_low
 	
-	#LOOP 5 vezes
+	sll $s4, $s4, 1		# shifta high do resto
+	or $s4, $s4, $t0	# faz or lógico para setar LSB do high para MSB do low
+	sll $s0, $s0, 1 	# shifta low do resto
+	
+	#LOOP 32 vezes
 	li $s3, 0
 	loop:
-        # Verifica se i < 4
-        bge $s3, 4, end_loop
-
-        # Corpo do loop (coloque aqui o código a ser repetido)
-	neg $t0, $s1 #$to = - divisor
-	add $s0, $s0, $t0 #Resto = Resto + (-Divisor)
-	#se resto > 0 -> pula, se não, resto = resto + divisor
-	bgt $s0, 0, nao_soma
+        # Verifica se i < 31
+        bge $s3, 32, end_loop
+        
+        bltu $s4, $s1, resto_negativo	# checa se resto é menor que divisor. Se for shifta resto para esquerda e lsb=0, se não, resto = resto - divisor e shifta resto para esquerda e lsb=1
+        
+        resto_positivo:
+        	subu $s4, $s4, $s1      # resto = resto + (-divisor)
+        
+        	srl $t0, $s0, 31	# pega MSB do resto_low
 	
-	add $s0, $s0, $s1
-	sll $s0, $s0, 1
-	j fim_if
+		sll $s4, $s4, 1		# shifta high do resto
+		or $s4, $s4, $t0	# faz or lógico para setar LSB do high para MSB do low
+		sll $s0, $s0, 1 	# shifta low do resto
+		
+		ori $s0, $s0, 1         #seta LSB = 1
+		
+		j fim_if
+        
+        resto_negativo:
+        	srl $t0, $s0, 31	# pega MSB do resto_low
 	
-	nao_soma:
-	sll $s0, $s0, 1
-	add $s0, $s0, 1
+		sll $s4, $s4, 1		# shifta high do resto
+		or $s4, $s4, $t0	# faz or lógico para setar LSB do high para MSB do low
+		sll $s0, $s0, 1 	# shifta low do resto
 	
 	fim_if:
-		
         # Incrementa i
         addi $s3, $s3, 1
-
         # Salta de volta para o início do loop
         j loop
     end_loop:
+    srl $s4, $s4, 1 #shift para direita do resto_high
     #por fim
-    la $t0, mascara_metade_direita
-    lw $t0, 0($t0)
-    and $t1, $t0, $s0
-    move $v0, $t1
+    move $v0, $s0 # quociente
+    move $v1, $s4 # resto
    #retomando pilha
     lw $s0, 4($sp)
     lw $s1, 8($sp)
     lw $s3, 12($sp)
+    lw $s4, 16($sp)
     lw $ra, 0($sp)
-    addiu $sp, $sp, 16
+    addiu $sp, $sp, 20
 #retornando à main
     jr $ra
 	
 
 
 .data
-    x: .word 0x90357274
-    y: .word 0x12341234
-    mascara_metade_esquerda: .word 0x000000f0
-    mascara_metade_direita: .word 0x0000000f
+    y: .word 0x90357274
+    x: .word 0x12341234
+    quociente: .word 0
+    resto: .word 0
+    quocienteString: .asciiz "\nO quociente eh: "
+    restoString: .asciiz "\nO resto eh: "
